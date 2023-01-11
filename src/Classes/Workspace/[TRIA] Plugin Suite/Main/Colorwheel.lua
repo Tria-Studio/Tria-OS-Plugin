3686da835118eef3ead350d3cab139837fb1ce83
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-01-11 02:45:37 -0600

    tagtype data structure for modular tag system

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Colorwheel.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Colorwheel.lua
@@ -388,8 +388,8 @@ function Colorwheel:GetUI()
                         BackgroundColor3 = Theme.InputFieldBackground.Default,
                         BorderColor3 = Theme.Border.Default,
                         BorderSizePixel = 1,
-                        Position = UDim2.new(.205 - .025, 0, .92, 0),
-                        Size = UDim2.new(.275, 0, .06, 0),
+                        Position = UDim2.new(.04, 0, .92, 0),
+                        Size = UDim2.new(.26, 0, .06, 0),
                         TextColor3 = Theme.MainText.Default,
                         PlaceholderColor3 = Theme.DimmedText.Default,
                         PlaceholderText = "Hex",
@@ -418,14 +418,6 @@ function Colorwheel:GetUI()
                             UpdateColor()
                         end
                     },
-                    New "TextLabel" { --// Hex textlabel
-                        BackgroundTransparency = 1,
-                        Position = UDim2.new(0.025, 0, 0.92, 0),
-                        Size = UDim2.new(0.163, 0, 0.06, 0),
-                        Font = Enum.Font.SourceSansSemibold,
-                        TextColor3 = Theme.SubText.Default,
-                        Text = "Hex:"
-                    }
                 }
             }
         }
