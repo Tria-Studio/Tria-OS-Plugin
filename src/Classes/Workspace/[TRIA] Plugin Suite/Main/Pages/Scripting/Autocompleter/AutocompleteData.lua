b2d19ae7345b62d93e156c8c39e54c56f0aff276
Krypt <kryptonforce102@gmail.com>
2023-02-12 09:22:21 +1100

    Todo

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/AutocompleteData.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/AutocompleteData.lua
@@ -2,6 +2,8 @@ local Data = {}
 Data.Properties = {branches = {}}
 Data.Methods = {branches = {}}
 
+-- TODO: Make this possibly use Github to fetch latest APIs which might be deprecated.
+
 for _, module in ipairs(script.Parent.API.Methods:GetChildren()) do
 	local moduleData = require(module)
 	Data.Methods.branches[module.Name] = moduleData
