a3066360b06e5c512b4b03ec025562124559d3ff
Krypt <kryptonforce102@gmail.com>
2023-02-20 19:44:08 +1100

    Unnecessary

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/GlobalSettings.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/GlobalSettings.lua
@@ -1,7 +1,3 @@
-local Package = script.Parent.Parent.Parent.Parent
-local Fusion = require(Package.Resources.Fusion)
-local Value = Fusion.Value
-
 local Settings = {}
 Settings.enabled = true
 Settings.runsInTriaScripts = false
