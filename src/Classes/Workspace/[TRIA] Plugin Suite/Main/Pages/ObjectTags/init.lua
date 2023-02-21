bbde1c0c879bc500658670654057cc48adaadca9
Krypt <kryptonforce102@gmail.com>
2023-02-22 09:42:14 +1100

    More typings

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/ObjectTags/init.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/ObjectTags/init.lua
@@ -47,7 +47,7 @@ function frame:GetFrame(data: PublicTypes.dictionary): Instance
 
                         [Children] = {
                             Components.Constraints.UIListLayout(),
-                            ForPairs(TagData.dataTypes.buttonTags, function(tagName, data)
+                            ForPairs(TagData.dataTypes.buttonTags, function(tagName: string, data: PublicTypes.dictionary): (string, Instance)
                                 return tagName, TagListener(tagName, data)
                             end, Fusion.cleanup)
                         },
@@ -65,7 +65,7 @@ function frame:GetFrame(data: PublicTypes.dictionary): Instance
 
                         [Children] = {
                             Components.Constraints.UIListLayout(),
-                            ForPairs(TagData.dataTypes.objectTags, function(tagName, data)
+                            ForPairs(TagData.dataTypes.objectTags, function(tagName: string, data: PublicTypes.dictionary): (string, Instance)
                                 return tagName, TagListener(tagName, data)
                             end, Fusion.cleanup)
                         },
