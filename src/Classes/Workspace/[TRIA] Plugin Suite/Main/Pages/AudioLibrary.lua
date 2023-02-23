0eebde5789ddda97f0e92541807cf0bf64191f92
KryptonForce102 <kryptonforce102@gmail.com>
2023-02-24 09:19:57 +1100

    Start building audio library ui

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
@@ -20,7 +20,18 @@ function frame:GetFrame(data: PublicTypes.dictionary): Instance
         Name = "ViewModes",
 
         [Children] = {
-            Components.PageHeader("Audio Library")
+            Components.PageHeader("Audio Library"),
+            Components.ScrollingFrame {
+                BackgroundColor3 = Theme.MainBackground.Default,
+                BackgroundTransparency = 1,
+                Size=  UDim2.fromScale(1, 1),
+
+                [Children] = {
+                    Components.Constraints.UIListLayout(nil, Enum.HorizontalAlignment.Center, UDim.new(0, 6)),
+                    Components.FrameHeader("About the Audio Library", 1, nil, nil, nil),
+                    Components.BasicTextLabel("Text Here", 2),
+                }
+            }
         }
     }
 end
