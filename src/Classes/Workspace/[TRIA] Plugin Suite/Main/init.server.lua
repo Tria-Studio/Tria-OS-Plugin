b63bd13c578f0f374a0305206d476ce8aef561d9
KryptonForce102 <kryptonforce102@gmail.com>
2023-02-23 12:49:59 +1100

    Whoops

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/init.server.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/init.server.lua
@@ -92,11 +92,11 @@ New "Frame" {
 
 			[OnEvent "InputChanged"] = function(InputObject: InputObject)
 				if InputObject.UserInputType == Enum.UserInputType.MouseWheel then
-					local newPage = Pages._currentPageNum:get(false) - InputObject.Position.Z
-					newPage = if newPage < 1 then #Pages._PageOrder elseif newPage > #Pages._PageOrder then 1 else newPage
+					local newPage = PageHandler._currentPageNum:get(false) - InputObject.Position.Z
+					newPage = if newPage < 1 then #PageHandler._PageOrder elseif newPage > #PageHandler._PageOrder then 1 else newPage
 
-					Pages._currentPageNum:set(newPage)
-					Pages:ChangePage(Pages._PageOrder[Pages._currentPageNum:get(false)])
+					PageHandler._currentPageNum:set(newPage)
+					PageHandler:ChangePage(PageHandler._PageOrder[PageHandler._currentPageNum:get(false)])
 				end
 			end,
 			[Children] = {
