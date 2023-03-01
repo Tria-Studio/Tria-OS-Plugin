7d1855584c24bf0e3e49f53971ad60e9afb6a752
KryptonForce102 <kryptonforce102@gmail.com>
2023-03-02 10:13:23 +1100

    Blank lines

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Resources/Components/Slider.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Resources/Components/Slider.lua
@@ -15,8 +15,6 @@ local Value = Fusion.Value
 local Ref = Fusion.Ref
 local Out = Fusion.Out
 
-
-
 return function (data: PublicTypes.Dictionary, holder: Instance): {Instance}
     local absolutePosition = Value(Vector2.zero)
     local absoluteSize = Value(Vector2.zero)
