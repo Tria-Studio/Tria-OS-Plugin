14151d6d3eaed4ab584fe87f8daf91bbba4ffd70
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-03-09 01:36:01 -0600

    minor UI change

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
@@ -389,12 +389,12 @@ function frame:GetFrame(data: PublicTypes.Dictionary): Instance
             Components.PageHeader("Audio Library"),
             SearchBox {
                 Position = UDim2.fromScale(0, 0),
-                Size = UDim2.new(0.5, 0, 0, 30),
+                Size = UDim2.new(0.5, 0, 0, 29),
                 Placeholder = "Search by Artist"
             },
             SearchBox {
                 Position = UDim2.fromScale(0.5, 0),
-                Size = UDim2.new(0.5, 0, 0, 30),
+                Size = UDim2.new(0.5, 0, 0, 29),
                 Placeholder = "Search by Name"
             },
             New "Frame" { -- Page Cycler
