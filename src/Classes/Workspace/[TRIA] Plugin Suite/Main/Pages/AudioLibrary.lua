ff6ee150a51fea23658e58c5a2d0112ac1151a11
Krypt <kryptonforce102@gmail.com>
2023-03-09 19:03:13 +1100

    Wrong clamp minimum

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
@@ -390,7 +390,7 @@ local function incrementPage(increment: number)
     local currentPage = pageData.current:get(false)
     local totalPages = pageData.total:get(false)
 
-    jumpToPage(math.clamp(currentPage + increment, 0, totalPages))
+    jumpToPage(math.clamp(currentPage + increment, 1, totalPages))
 end
 
 function frame:GetFrame(data: PublicTypes.Dictionary): Instance
