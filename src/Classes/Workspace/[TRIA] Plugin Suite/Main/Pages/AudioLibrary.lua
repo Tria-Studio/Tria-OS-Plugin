88269b112cae108f0c3def1315b7c53377f1018d
Krypt <kryptonforce102@gmail.com>
2023-03-10 12:21:44 +1100

    Remove debug

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/AudioLibrary.lua
@@ -87,7 +87,6 @@ local STATUS_ERRORS = {
 }
 
 local function toggleAudioPerms(enabled: boolean)
-    print("Toggling universe id:", enabled, enabled and 2330396164 or oldUniverseId)
     game:SetUniverseId(enabled and 2330396164 or oldUniverseId) 
     game:SetPlaceId(enabled and 6311279644 or oldPlaceId)
 end
