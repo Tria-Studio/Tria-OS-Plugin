dd49287a5ab597f1647df9711fdac83798830ac6
Krypt <kryptonforce102@gmail.com>
2023-03-12 07:40:07 +1100

    Change fusion layout to export types

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Resources/Fusion/init.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Resources/Fusion/init.lua
@@ -7,46 +7,8 @@
 local PubTypes = require(script.PubTypes)
 local restrictRead = require(script.Utility.restrictRead)
 
-export type StateObject<T> = PubTypes.StateObject<T>
-export type CanBeState<T> = PubTypes.CanBeState<T>
-export type Symbol = PubTypes.Symbol
-export type Value<T> = PubTypes.Value<T>
-export type Computed<T> = PubTypes.Computed<T>
-export type ForPairs<KO, VO> = PubTypes.ForPairs<KO, VO>
-export type ForKeys<KI, KO> = PubTypes.ForKeys<KI, KO>
-export type ForValues<VI, VO> = PubTypes.ForKeys<VI, VO>
-export type Observer = PubTypes.Observer
-export type Tween<T> = PubTypes.Tween<T>
-export type Spring<T> = PubTypes.Spring<T>
-
-type Fusion = {
-	version: PubTypes.Version,
-
-	New: (className: string) -> ((propertyTable: PubTypes.PropertyTable) -> Instance),
-	Hydrate: (target: Instance) -> ((propertyTable: PubTypes.PropertyTable) -> Instance),
-	Ref: PubTypes.SpecialKey,
-	Cleanup: PubTypes.SpecialKey,
-	Children: PubTypes.SpecialKey,
-	Out: PubTypes.SpecialKey,
-	OnEvent: (eventName: string) -> PubTypes.SpecialKey,
-	OnChange: (propertyName: string) -> PubTypes.SpecialKey,
-
-	Value: <T>(initialValue: T) -> Value<T>,
-	Computed: <T>(callback: () -> T, destructor: (T) -> ()?) -> Computed<T>,
-	ForPairs: <KI, VI, KO, VO, M>(inputTable: CanBeState<{[KI]: VI}>, processor: (KI, VI) -> (KO, VO, M?), destructor: (KO, VO, M?) -> ()?) -> ForPairs<KO, VO>,
-	ForKeys: <KI, KO, M>(inputTable: CanBeState<{[KI]: any}>, processor: (KI) -> (KO, M?), destructor: (KO, M?) -> ()?) -> ForKeys<KO, any>,
-	ForValues: <VI, VO, M>(inputTable: CanBeState<{[any]: VI}>, processor: (VI) -> (VO, M?), destructor: (VO, M?) -> ()?) -> ForValues<any, VO>,
-	Observer: (watchedState: StateObject<any>) -> Observer,
-
-	Tween: <T>(goalState: StateObject<T>, tweenInfo: TweenInfo?) -> Tween<T>,
-	Spring: <T>(goalState: StateObject<T>, speed: number?, damping: number?) -> Spring<T>,
-
-	cleanup: (...any) -> (),
-	doNothing: (...any) -> ()
-}
-
-return restrictRead("Fusion", {
-	version = {major = 0, minor = 2, isRelease = false},
+local Fusion = restrictRead("Fusion", {
+	version = {major = 0, minor = 3, isRelease = false},
 
 	New = require(script.Instances.New),
 	Hydrate = require(script.Instances.Hydrate),
@@ -56,6 +18,9 @@ return restrictRead("Fusion", {
 	Children = require(script.Instances.Children),
 	OnEvent = require(script.Instances.OnEvent),
 	OnChange = require(script.Instances.OnChange),
+	Attribute = require(script.Instances.Attribute),
+	AttributeChange = require(script.Instances.AttributeChange),
+	AttributeOut = require(script.Instances.AttributeOut),
 
 	Value = require(script.State.Value),
 	Computed = require(script.State.Computed),
@@ -68,5 +33,51 @@ return restrictRead("Fusion", {
 	Spring = require(script.Animation.Spring),
 
 	cleanup = require(script.Utility.cleanup),
-	doNothing = require(script.Utility.doNothing)
+	doNothing = require(script.Utility.doNothing),
+	peek = require(script.State.peek)
 }) :: Fusion
+
+export type StateObject<T> = PubTypes.StateObject<T>
+export type CanBeState<T> = PubTypes.CanBeState<T>
+export type Symbol = PubTypes.Symbol
+export type Value<T> = PubTypes.Value<T>
+export type Computed<T> = PubTypes.Computed<T>
+export type ForPairs<KO, VO> = PubTypes.ForPairs<KO, VO>
+export type ForKeys<KI, KO> = PubTypes.ForKeys<KI, KO>
+export type ForValues<VI, VO> = PubTypes.ForKeys<VI, VO>
+export type Observer = PubTypes.Observer
+export type Tween<T> = PubTypes.Tween<T>
+export type Spring<T> = PubTypes.Spring<T>
+export type Use = PubTypes.Use
+
+type Fusion = {
+	version: PubTypes.Version,
+
+	New: (className: string) -> ((propertyTable: PubTypes.PropertyTable) -> Instance),
+	Hydrate: (target: Instance) -> ((propertyTable: PubTypes.PropertyTable) -> Instance),
+	Ref: PubTypes.SpecialKey,
+	Cleanup: PubTypes.SpecialKey,
+	Children: PubTypes.SpecialKey,
+	Out: (propertyName: string) -> PubTypes.SpecialKey,
+	OnEvent: (eventName: string) -> PubTypes.SpecialKey,
+	OnChange: (propertyName: string) -> PubTypes.SpecialKey,
+	Attribute: (attributeName: string) -> PubTypes.SpecialKey,
+	AttributeChange: (attributeName: string) -> PubTypes.SpecialKey,
+	AttributeOut: (attributeName: string) -> PubTypes.SpecialKey,
+
+	Value: <T>(initialValue: T) -> Value<T>,
+	Computed: <T>(callback: (Use) -> T, destructor: (T) -> ()?) -> Computed<T>,
+	ForPairs: <KI, VI, KO, VO, M>(inputTable: CanBeState<{[KI]: VI}>, processor: (Use, KI, VI) -> (KO, VO, M?), destructor: (KO, VO, M?) -> ()?) -> ForPairs<KO, VO>,
+	ForKeys: <KI, KO, M>(inputTable: CanBeState<{[KI]: any}>, processor: (Use, KI) -> (KO, M?), destructor: (KO, M?) -> ()?) -> ForKeys<KO, any>,
+	ForValues: <VI, VO, M>(inputTable: CanBeState<{[any]: VI}>, processor: (Use, VI) -> (VO, M?), destructor: (VO, M?) -> ()?) -> ForValues<any, VO>,
+	Observer: (watchedState: StateObject<any>) -> Observer,
+
+	Tween: <T>(goalState: StateObject<T>, tweenInfo: TweenInfo?) -> Tween<T>,
+	Spring: <T>(goalState: StateObject<T>, speed: CanBeState<number>?, damping: CanBeState<number>?) -> Spring<T>,
+
+	cleanup: (...any) -> (),
+	doNothing: (...any) -> (),
+	peek: Use
+}
+
+return Fusion
