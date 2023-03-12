2efbdca1c140d1da6a20d8cb40988504a6325e3f
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-03-11 18:48:37 -0600

    update default page

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/MenuData.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/MenuData.lua
@@ -25,12 +25,12 @@ return {
 	},
 
 	Pages = {
-		{Name = "ObjectTags"},
+		{Name = "ObjectTags", Default = true},
 		{Name = "ViewModes"},
 		{Name = "Settings"},
 		{Name = "Scripting"},
 		{Name = "Publish"},
 		{Name = "Insert"},
-		{Name = "AudioLibrary", Default = true}
+		{Name = "AudioLibrary"}
 	}
 }
