b6ed6b26e78b3b01b97d4d9fad7b4d801e4b70dd
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-03-11 19:25:13 -0600

    update waterjet instructions

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Insert/AddonFiles/Waterjets/init.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Insert/AddonFiles/Waterjets/init.lua
@@ -11,13 +11,14 @@
         - Follow the following naming scheme: _WaterJet[you can put whatever after this]
         - The FRONT face is the face that becomes the jetstream
     	
-    FanNumber [number]: A unique identifier for the fan in use of scripting.
-    FanSpeed [number]: Speed in studs/sec that the fan will push players inside its bounds.
-    Distance [number]: The total distance that the fan can push you.
-    LinearMovement [boolean]: When true, the fans push players at a constant rate throughout the entire distance. When false, the rate that the fan pushes players decreases as you get farther away.
-    FanShape [string]: Determines if the bounds that the fan will push players in is a cylinder, or a box.
-    Enabled [boolean]: Determines whether the fan is currently active.
-    BubbleParticle [number]: The number of the ImageID that the fan's particles will show. 
+	Each Fan must have the following attributes:
+		FanNumber [number]: A unique identifier for the fan in use of scripting.
+		FanSpeed [number]: Speed in studs/sec that the fan will push players inside its bounds.
+		Distance [number]: The total distance that the fan can push you.
+		LinearMovement [boolean]: When true, the fans push players at a constant rate throughout the entire distance. When false, the rate that the fan pushes players decreases as you get farther away.
+		FanShape [string]: Determines if the bounds that the fan will push players in is a cylinder, or a box.
+		Enabled [boolean]: Determines whether the fan is currently active.
+		BubbleParticle [number]: The number of the ImageID that the fan's particles will show. 
     	
     To toggle fan states easily, you can use the function below:
    		function module:SetFanEnabled(Fan: Part, Enabled: boolean)
