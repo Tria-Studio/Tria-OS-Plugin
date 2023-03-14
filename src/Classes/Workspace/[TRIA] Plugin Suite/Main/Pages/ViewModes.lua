916cccc0c7d0c88261db93d336b7f444ce1577b6
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-03-13 19:14:01 -0500

    work on view mode page activation

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/ViewModes.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/ViewModes.lua
@@ -10,10 +10,16 @@ local Pages = require(Resources.Components.Pages)
 
 local New = Fusion.New
 local Children = Fusion.Children
-local Computed = Fusion.Computed
+local Observer = Fusion.Observer
+local Value = Fusion.Value
+
+local PAGE_ACTIVE = Value(false)
+
+local TextLabelVisible = Value(true)
 
 local frame = {}
- 
+
+
 function frame:GetFrame(data: PublicTypes.Dictionary): Instance
     return New "Frame" {
         Size = UDim2.fromScale(1, 1),
@@ -23,10 +29,7 @@ function frame:GetFrame(data: PublicTypes.Dictionary): Instance
 
         [Children] = {
             Components.PageHeader("View Modes"),
-            Components.GradientTextLabel(Computed(function()
-                local mapModel = Util.mapModel:get()
-                return not Util.hasSpecialFolder:get() and mapModel
-            end), {
+            Components.GradientTextLabel(TextLabelVisible, {
                 Size = UDim2.new(1, 0, 1, 0),
                 Text = "Unsupported Map."
             })
@@ -34,19 +37,31 @@ function frame:GetFrame(data: PublicTypes.Dictionary): Instance
     }
 end
 
-Util.MapChanged:Connect(function()
-    if not Util.hasSpecialFolder:get() then
-        Util:ShowMessage("Feature Unavaliable", "Due to the complexity and performance, View Modes only supports maps with OptimizedStructure (aka the \"Special\" folder). You can add this to your map at the insert page.", {Text = "Get Mapkit", Callback = function()
+local function UpdatePage()
+    local old = PAGE_ACTIVE:get()
+    PAGE_ACTIVE:set(Util.hasSpecialFolder:get() and Util.mapModel:get())
+    TextLabelVisible:set(not PAGE_ACTIVE:get())
+    
+    if old and not PAGE_ACTIVE:get() and Pages.pageData.currentPage:get() == "ViewModes" then
+        Util:ShowMessage("Feature Unavaliable, AAAAA", "Due to the complexity and performance, View Modes only supports maps with OptimizedStructure (aka the \"Special\" folder). You can add this to your map at the insert page.", {Text = "Get OptimizedStructure", Callback = function()
             Pages:ChangePage("Insert")
         end})
     end
-end)
+end
+Observer(Util.hasSpecialFolder):onChange(UpdatePage)
+Observer(Util.mapModel):onChange(UpdatePage)
 
-function frame.onOpen()
+function frame.OnOpen()
     if not Util.hasSpecialFolder:get() and Util.mapModel:get() then
-        Util:ShowMessage("Feature Unavaliable", "Due to the complexity and performance, View Modes only supports maps with OptimizedStructure (aka the \"Special\" folder). You can add this to your map at the insert page.", {Text = "Get Mapkit", Callback = function()
+        Util:ShowMessage("Feature Unavaliable", "Due to the complexity and performance, View Modes only supports maps with OptimizedStructure (aka the \"Special\" folder). You can add this to your map at the insert page.", {Text = "Get OptimizedStructure", Callback = function()
             Pages:ChangePage("Insert")
         end})
+    end
+end
+
+function frame.OnClose()
+    if Util._Message.Header:get() == "Feature Unavaliable" then
+        Util.CloseMessage() 
     end
 end
 
