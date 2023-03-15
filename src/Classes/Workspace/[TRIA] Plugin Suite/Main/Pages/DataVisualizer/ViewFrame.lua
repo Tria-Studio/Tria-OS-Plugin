482640a8eca89c69f105359f8bf6cfcf30740e3e
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-03-15 17:05:03 -0500

    fuck merges

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/DataVisualizer/ViewFrame.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/DataVisualizer/ViewFrame.lua
@@ -19,78 +19,91 @@ return function(name: string, data: PublicTypes.Dictionary)
 
     return New "Frame" {
         AutomaticSize = Enum.AutomaticSize.Y,
-        BackgroundColor3 = Theme.Dropdown.Default,
+        BackgroundColor3 = Theme.ScrollBarBackground.Default,
         LayoutOrder = data.LayoutOrder,
         Size = UDim2.new(1, 0, 0, 4),
         Name = name,
 
-        [Children] = New "Frame" {
-            BackgroundColor3 = Theme.Button.Default,    
-            Size = UDim2.new(1, 0, 0, 25),
-            AutomaticSize = Enum.AutomaticSize.Y,
+        [Children] = {
+            Components.Constraints.UIPadding(nil, nil, UDim.new(0, 56), nil),
+            New "TextButton" { --// Button
+                AutoButtonColor = Computed(function(): boolean
+                    local interfaceActive = Util.interfaceActive:get()
+                    return if Util.dropdownActive:get() then false else interfaceActive
+                end),
+                Active = Util.interfaceActive,
+                BackgroundColor3 = Theme.Button.Default,
+                Position = UDim2.fromOffset(-56, 0),
+                Size = UDim2.new(1, 56, 0, 25),
+                Font = Enum.Font.SourceSansBold,
+                Text = data.DisplayText,
+                TextColor3 = Theme.MainText.Default,
+                TextXAlignment = Enum.TextXAlignment.Left,
 
-            [Children] = {
-                Components.Constraints.UIPadding(nil, nil, UDim.new(0, 56), nil),
-                New "TextButton" { --// Button
-                    AutoButtonColor = Computed(function(): boolean
-                        local interfaceActive = Util.interfaceActive:get()
-                        return if Util.dropdownActive:get() then false else interfaceActive
-                    end),
-                    Active = Util.interfaceActive,
-                    BackgroundColor3 = Theme.Button.Default,
-                    Position = UDim2.fromOffset(-56, 0),
-                    Size = UDim2.new(1, 56, 0, 25),
-                    Font = Enum.Font.SourceSansBold,
-                    Text = data.DisplayText,
-                    TextColor3 = Theme.MainText.Default,
-                    TextXAlignment = Enum.TextXAlignment.Left,
+                [Children] = Components.Constraints.UIPadding(nil, nil, UDim.new(0, 56), nil)
+            },
+            Components.Checkbox(20, UDim2.fromOffset(-30, 2), Vector2.new(1, 0), checkState),
+            New "ImageLabel" { --// Icon
+                Size = UDim2.fromOffset(20, 20),
+                Position = UDim2.fromOffset(-6, 2),
+                BackgroundTransparency = 1,
+                AnchorPoint = Vector2.new(1, 0),
+                Image = data.DisplayIcon,
+            },
+            Components.TooltipImage {
+                Header = if data.Tooltip.Text == "" then nil else data.Tooltip.Header,
+                Tooltip = if data.Tooltip.Text == "" then nil else data.Tooltip.Text,
+                Position = UDim2.new(1, -4, 0, 4)
+            },
+            New "Frame" {
+                Position = UDim2.new(0, -56, 0, 25),
+                AutomaticSize = Enum.AutomaticSize.Y,
+                Size = UDim2.new(1, 56, 0, 0),
+                BackgroundTransparency = 1,
+                LayoutOrder = 2,
 
-                    [Children] = Components.Constraints.UIPadding(nil, nil, UDim.new(0, 56), nil)
-                },
-                Components.Checkbox(20, UDim2.fromOffset(-30, 2), Vector2.new(1, 0), checkState),
-                New "ImageLabel" { --// Icon
-                    Size = UDim2.fromOffset(20, 20),
-                    Position = UDim2.fromOffset(-6, 2),
-                    BackgroundTransparency = 1,
-                    AnchorPoint = Vector2.new(1, 0),
-                    Image = data.DisplayIcon,
-                },
-                Components.TooltipImage {
-                    Header = data.Tooltip.Header,
-                    Tooltip = data.Tooltip.Text,
-                    Position = UDim2.new(1, -4, 0, 4)
-                },
-                New "Frame" {
-                    Position = UDim2.new(0, -56, 0, 25),
-                    AutomaticSize = Enum.AutomaticSize.Y,
-                    Size = UDim2.new(1, 56, 0, 0),
-                    BackgroundTransparency = 1,
-                    LayoutOrder = 2,
+                [Children] = {
+                    Components.Constraints.UIListLayout(),
+                    Components.Spacer(data.SingleOption, 0, 2, 1, Theme.ScrollBarBackground.Default),
+                    New "TextLabel" {
+                        Text = data.SubText,
+                        Visible = #data.ViewOptions > 0,
+                        BackgroundColor3 = Theme.MainBackground.Default,
+                        BorderColor3 = Theme.Border.Default,
+                        BorderSizePixel = 1,
+                        Size = UDim2.new(1, 0, 0, 22),
+                        Font = Enum.Font.SourceSansSemibold,
+                        TextColor3 = Theme.BrightText.Default,
+                        TextSize = 16
+                    },
+                    ForValues(data.ViewOptions, function(metadata: PublicTypes.Dictionary): Instance
+                        local dataValue = Value(false)
+                        return New "TextButton" {
+                            BackgroundColor3 = Theme.ScrollBarBackground.Default,
+                            BorderColor3 = Theme.Border.Default,
+                            BorderSizePixel = 1,
+                            Size = UDim2.new(1, 0, 0, 22),
+                            LayoutOrder = metadata.LayoutOrder + 1,
+                            Text = " " .. metadata.Name,
+                            TextColor3 = Theme.MainText.Default,
+                            Font = Enum.Font.SourceSansSemibold,
+                            TextSize = 15,
+                            TextXAlignment = Enum.TextXAlignment.Left,
 
-                    [Children] = {
-                        Components.Constraints.UIListLayout(),
-                        ForValues(data.ViewOptions, function(metadata: PublicTypes.Dictionary): Instance
-                            local dataValue = Value(false)
-    
-                            return New "TextButton" {
-                                BackgroundColor3 = Theme.ScrollBarBackground.Default,
-                                BorderColor3 = Theme.Border.Default,
-                                BorderSizePixel = 1,
-                                Size = UDim2.new(1, 0, 0, 22),
-                                LayoutOrder = metadata.LayoutOrder,
-                                Text = " " .. metadata.Name,
-                                TextColor3 = Theme.MainText.Default,
-                                Font = Enum.Font.SourceSansSemibold,
-                                TextSize = 15,
-                                TextXAlignment = Enum.TextXAlignment.Left,
-    
-                                [Children] = {
-                                    Components.Constraints.UIPadding(nil, nil, UDim.new(0, 22)),
-                                    Components.Checkbox(18, UDim2.fromOffset(-2, 2), Vector2.new(1, 0), dataValue)
-                                }
+                            [Children] = {
+                                Components.Constraints.UIPadding(nil, nil, UDim.new(0, 44)),
+                                Components.Checkbox(18, UDim2.fromOffset(-24, 2), Vector2.new(1, 0), dataValue),
+                                New "ImageLabel" { --// Icon
+                                    Size = UDim2.fromOffset(18, 18),
+                                    Position = UDim2.fromOffset(-2, 2),
+                                    BackgroundTransparency = 1,
+                                    AnchorPoint = Vector2.new(1, 0),
+                                    Image = metadata.DisplayIcon,
+                                },
                             }
-                        end)
-                    }
+                        }
+                    end),
+                    Components.Spacer(data.SingleOption, #data.ViewOptions + 2, 2, 1, Theme.ScrollBarBackground.Default),   
                 }
             }
         }
