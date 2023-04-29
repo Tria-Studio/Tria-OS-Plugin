faf94d6fb1db5dcb803e24ed1ad7cc371b483d28
Krypt <kryptonforce102@gmail.com>
2023-04-29 19:48:15 +1000

    Finalise some more

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/API/Methods/GetFeature.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Scripting/Autocompleter/API/Methods/GetFeature.lua
@@ -1,24 +1,50 @@
 local possibleBranches = {
-	["ToggleSliding"] = {
-		Name = "ToggleSliding",
-		Parameters = {"true", "false"},
-		Documentation = {
-			value = "Toggles sliding"
+	["Skills"] = {
+		["ToggleSliding"] = {
+			Name = "ToggleSliding",
+			Parameters = {"true", "false"},
+			Documentation = {
+				value = "Toggles sliding"
+			},
+			CodeSample = [[
+	MapLib:GetFeature("Skills"):ToggleSliding(true)	
+			]]
 		},
-		CodeSample = [[
-MapLib:GetFeature("Skills"):ToggleSliding(true)	
-		]]
+	},
+
+	["Settings"] = {
+		["GetSetting"] = {
+			Name = "GetSetting",
+			Parameters = {},
+			Documentation = {
+				value = "Retrieves a setting"
+			},
+			CodeSample = [[
+	MapLib:GetFeature("Settings"):GetSetting("FOV")	
+			]]
+		}
 	}
 }
+
+local possibleBranchArray = {}
+for _, v in pairs(possibleBranches) do
+	for k, d in pairs(v) do
+		possibleBranchArray[k] = d
+	end
+end
 
 return {
 	AutocompleteArgs = {"featureName"},
 	Name = "GetFeature",
-	PossibleBranches = possibleBranches,
+	PossibleBranches = possibleBranchArray,
 	Branches = function(params)
 		local features = {
 			["Skills"] = {
-				["ToggleSliding"] = possibleBranches.ToggleSliding
+				["ToggleSliding"] = possibleBranches.Skills.ToggleSliding
+			},
+
+			["Settings"] = {
+				["GetSetting"] = possibleBranches.Settings.GetSetting
 			},
 		}
 		
