72a9018bab1301b59461794f20eb54bbb7448d75
grif-0 <61262583+grif-0@users.noreply.github.com>
2023-06-07 01:23:32 -0500

    added air dive setting

--- src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Settings/SettingData/DataModules/Skills.lua
+++ src/Classes/Workspace/[TRIA] Plugin Suite/Main/Pages/Settings/SettingData/DataModules/Skills.lua
@@ -12,7 +12,7 @@ local Data = {
 }
 
 Data.Items = {
-	{
+	{ 
 		Text = "Allow Sliding",
 		Type = "Checkbox",
 
@@ -39,6 +39,20 @@ Data.Items = {
 			Text = [[Determines the type of sliding which is used in the map.
 <b>Linear: </b>Sliding speed is constant throughout the duration of the slide.
 <b>Non-linear: </b>Sliding speed is based on two combined tweens which both speedup and slowdown the player.]],
+		},
+	},
+
+	{ 
+		Text = "Allow Air Dive",
+		Type = "Checkbox",
+
+		Directory = "Skills",
+		Attribute = "AllowAirDive",
+		Fallback = true,
+
+		Value = Value(false),
+		Tooltip = {
+			Text = "Determines whether or not players are allowed to air dive in the map",
 		},
 	},
 }
